36813849cd5f30ce9f22abe9183a65e64e19f1bf
Rishi <rishimaheshwari040@gmail.com>
2025-02-09 18:13:52 +0530

    up

--- src/components/core/Home/Slider.jsx
+++ src/components/core/Home/Slider.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import banner from "../../../assets/banner1.jpg";
+import banner2 from "../../../assets/banner2.png";
 import Image from "next/image";
 import ContactCom from "./ContactCom";
 
@@ -9,7 +10,12 @@ const Slider = () => {
       <Image
         src={banner}
         alt="not found"
-        className="w-full lg:h-[85vh] object-fill"
+        className="w-full hidden lg:block lg:h-[85vh] object-fill"
+      />
+      <Image
+        src={banner2}
+        alt="not found"
+        className="w-full block lg:hidden lg:h-[85vh] object-fill"
       />
       <div className="w-full  lg:max-w-md p- bg-white/60 shadow-lg rounded-lg mt-4 lg:mt-0 lg:absolute lg:top-1/2 lg:right-4 lg:-translate-y-1/2">
         <ContactCom />
